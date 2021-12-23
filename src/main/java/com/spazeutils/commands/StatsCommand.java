c6bdd54189db345839ba60e329fa15594fb5125d
Theminestone <gabriel.unterweger05@gmail.com>
2021-12-24 00:02:47 +0100

    edited Stats

--- src/main/java/com/spazeutils/commands/StatsCommand.java
+++ src/main/java/com/spazeutils/commands/StatsCommand.java
@@ -6,16 +6,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import java.util.Arrays;
-
 public class StatsCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage(Main.prefix + "Total Uptime: " + Main.getPlugin().getConfig().getInt("stats.uptime_total") + " Minutes");
-        sender.sendMessage(Main.prefix + "Uptime: " + Main.getPlugin().getConfig().getInt("stats.uptime") + " Minutes");
-        sender.sendMessage(Main.prefix + "TPS: " + Arrays.toString(MinecraftServer.getServer().recentTps));
-        sender.sendMessage(Main.prefix + "Memory Usage: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
-        sender.sendMessage(Main.prefix + "Available Processors: " + Runtime.getRuntime().availableProcessors());
+        sender.sendMessage("--------" + Main.prefix + "--------");
+        sender.sendMessage("Total Uptime: " + Main.getPlugin().getConfig().getInt("stats.uptime_total") + " Minutes");
+        sender.sendMessage("Uptime: " + Main.getPlugin().getConfig().getInt("stats.uptime") + " Minutes");
+        sender.sendMessage("TPS: " + MinecraftServer.TPS);
+        sender.sendMessage("Memory Usage: " + Runtime.getRuntime().freeMemory());
+        sender.sendMessage("Available Processors: " + Runtime.getRuntime().availableProcessors());
         return false;
     }
 }
--- src/main/java/com/spazeutils/listeners/PlayerQuit.java
+++ src/main/java/com/spazeutils/listeners/PlayerQuit.java
@@ -2,11 +2,13 @@ package com.spazeutils.listeners;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuit implements Listener {
 
+    @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
 
         Player player = event.getPlayer();
