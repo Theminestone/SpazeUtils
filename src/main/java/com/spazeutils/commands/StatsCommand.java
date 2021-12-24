abfe124bcf1d66cfa7d1edd197c129882f736b2d
Theminestone <gabriel.unterweger05@gmail.com>
2021-12-24 15:30:18 +0100

    added InfoCommand

--- src/main/java/com/spazeutils/commands/StatsCommand.java
+++ src/main/java/com/spazeutils/commands/StatsCommand.java
@@ -2,6 +2,7 @@ package com.spazeutils.commands;
 
 import com.spazeutils.Main;
 import net.minecraft.server.MinecraftServer;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,24 +12,15 @@ public class StatsCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-//        sender.sendMessage(ChatColor.GOLD + "-------- " + ChatColor.RESET + Main.prefix + ChatColor.GOLD + "--------");
-//        sender.sendMessage("Total Uptime: " + Main.getPlugin().getConfig().getInt("stats.uptime_total") + " Minutes");
-//        sender.sendMessage("Uptime: " + Main.getPlugin().getConfig().getInt("stats.uptime") + " Minutes");
-//        sender.sendMessage("TPS: " + MinecraftServer.TPS + " Ticks per Second");
-//        sender.sendMessage("Free Memory: " + (Runtime.getRuntime().freeMemory() / 1000000) + " MB"); // or 1024000 ?
-//        sender.sendMessage("Total Memory: " + (Runtime.getRuntime().totalMemory() / 1000000) + " MB"); // or 1024000 ?
-//        sender.sendMessage("Used Memory: " + (((int) Runtime.getRuntime().totalMemory() - (int) Runtime.getRuntime().freeMemory()) / 1000000) + " MB"); // or 1024000 ?
-//        sender.sendMessage("Available Processors: " + Runtime.getRuntime().availableProcessors());
-
-        //TODO edit Design
-        sender.sendMessage(ChatColor.GOLD + "------" + ChatColor.RESET + Main.prefix + ChatColor.GOLD + "-----\n" + ChatColor.AQUA +
-                "Total Uptime: " + ChatColor.BOLD + Main.getPlugin().getConfig().getInt("stats.uptime_total") + ChatColor.AQUA + " Minutes\n" +
-                "Uptime: " + ChatColor.BOLD + Main.getPlugin().getConfig().getInt("stats.uptime") + ChatColor.AQUA + " Minutes\n" +
-                "TPS: " + ChatColor.BOLD + MinecraftServer.TPS + ChatColor.AQUA + " Ticks per Second\n" +
-                "Free Memory: " + ChatColor.BOLD + (Runtime.getRuntime().freeMemory() / 1000000) + ChatColor.AQUA + " MB\n" +
-                "Total Memory: " + ChatColor.BOLD + (Runtime.getRuntime().totalMemory() / 1000000) + ChatColor.AQUA + " MB\n" +
-                "Used Memory: " + ChatColor.BOLD + (((int) Runtime.getRuntime().totalMemory() - (int) Runtime.getRuntime().freeMemory()) / 1000000) + ChatColor.AQUA + " MB\n" +
-                "Available Processors: " + ChatColor.BOLD + Runtime.getRuntime().availableProcessors());
+        sender.sendMessage(ChatColor.GOLD + "-------- " + ChatColor.RESET + Main.prefix + ChatColor.GOLD + "--------" + ChatColor.RESET);
+        sender.sendMessage(ChatColor.AQUA + "Total Uptime:    " + ChatColor.GREEN + Main.getPlugin().getConfig().getInt("stats.uptime_total") + " Minutes");
+        sender.sendMessage(ChatColor.AQUA + "Uptime:            " + ChatColor.GREEN + Main.getPlugin().getConfig().getInt("stats.uptime") + " Minutes");
+        sender.sendMessage(ChatColor.AQUA + "TPS:               " + ChatColor.GREEN + MinecraftServer.TPS + " Ticks");
+        sender.sendMessage(ChatColor.AQUA + "Free Memory:   " + ChatColor.GREEN + (Runtime.getRuntime().freeMemory() / 1000000) + " MB"); // or 1024000 ?
+        sender.sendMessage(ChatColor.AQUA + "Total Memory:   " + ChatColor.GREEN + (Runtime.getRuntime().totalMemory() / 1000000) + " MB"); // or 1024000 ?
+        sender.sendMessage(ChatColor.AQUA + "Used Memory:   " + ChatColor.GREEN + (((int) Runtime.getRuntime().totalMemory() - (int) Runtime.getRuntime().freeMemory()) / 1000000) + " MB"); // or 1024000 ?
+        sender.sendMessage(ChatColor.AQUA + "Processors:    " + ChatColor.GREEN + Runtime.getRuntime().availableProcessors());
+        sender.sendMessage(ChatColor.AQUA + "Online " + Bukkit.getServer().getOnlinePlayers().size());
 
         return false;
     }
